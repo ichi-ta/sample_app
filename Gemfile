7cb0785b5e57d2ac7f5c6829430a87fb85d9c297
isshin <gitcommit.for@gmail.com>
2022-04-03 23:00:29 +0900

    Add secure password

--- Gemfile
+++ Gemfile
@@ -2,9 +2,10 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '6.1.5'
+gem 'rails',          '6.1.5'
 gem 'bootstrap-sass', '3.3.7'
 gem 'rails-controller-testing'
+gem 'bcrypt',         '3.1.12'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,13 +5,16 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
+  #有効性
   test "should be valid" do
     assert @user.valid?
   end
 
+  #存在性
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
@@ -22,6 +25,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  #長さ
   test "name should not be too long" do
     @user.name = "a" * 51
     assert_not @user.valid?
@@ -32,6 +36,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  #フォーマット
   test "email validation should reject invalid address" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |valid_addresses|
@@ -40,10 +45,22 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  #一意性
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  #パスワードの最小文字数
+  test "password should be present (nonbrank)" do
+    @user.password = @user.password_confirmation =" " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
