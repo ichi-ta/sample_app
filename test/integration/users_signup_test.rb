92d637c861287f82d7a58451acd94de04bba0605
isshin <gitcommit.for@gmail.com>
2022-04-10 18:08:25 +0900

    Finish login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,8 +7,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_no_difference 'User.count' do
       post signup_path, params: {
         user: {
-          name: "",
-          email: "user@invalid",
+          name:                  "",
+          email:                 "user@invalid",
           password:              "foo",
           password_confirmation: "bar"
         }
@@ -24,8 +24,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_difference 'User.count', 1 do
       post users_path, params: {
         user: {
-          name: "Example user",
-          email: "user@example.com",
+          name:                  "Example user",
+          email:                 "user@example.com",
           password:              "password",
           password_confirmation: "password"
         }
@@ -33,6 +33,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.empty?
+    assert is_logged_in?
   end
 end
