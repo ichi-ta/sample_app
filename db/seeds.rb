95861d06c75ebbd9558f268df9aea7aa8d504e95
isshin <gitcommit.for@gmail.com>
2022-09-16 11:35:05 +0900

    done follow and unfollow interface (14.2)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!( name:                  "Example User",
-              email:                 "example@tutorial.org",
+              email:                 "example@rtutorial.org",
               password:              "foobar",
               password_confirmation: "foobar",
               admin: true )
@@ -26,3 +26,10 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+following.each { |follower| follower.follow(user) }
